82f7f88c1563d3411e89fe7d7a49786cf0963ab1
Ivan <vanesiotanaev@mail.ru>
2022-10-14 16:29:19 +0300

    basic rules

---
+++ controller.py
@@ -0,0 +1,9 @@
+import model
+import view
+
+def button_click():
+    value_a = view.get_value()
+    value_b = view.get_value()
+    model.init(value_a, value_b)
+    result = model.sum()
+    view.view(result)
---
+++ view.py
@@ -0,0 +1,5 @@
+def view(data):
+    print(data)
+
+def get_value():
+    return int(input('value = '))
